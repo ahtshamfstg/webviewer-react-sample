c8385b6ddf665ff8f917f444b20d8abff20e884e
Ahtsham Raziq <ahtsham.raziq@gmail.com>
2024-03-06 04:24:48 +0500

    repro sticky annotation locations on mixed page sizes

--- src/App.js
+++ src/App.js
@@ -5,32 +5,45 @@ import './App.css';
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  const licenseKey = '';
+
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
+        // load non-linearized file to see the expected annotation locations
+        // initialDoc: '/files/mixed-pages.pdf',
+        initialDoc: '/files/mixed-pages-linearized.pdf',
+        licenseKey,
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      const { documentViewer, annotationManager } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      documentViewer.addEventListener("documentLoaded", async () => {
+        const stickyAnnotations = [
+          '<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><fields /><add><text xmlns="http://ns.adobe.com/xfdf/" page="1" rect="1427.922,3525.276,1458.922,3556.276" color="#FFFF00" flags="print,nozoom,norotate" name="d41a26eb-72d2-1b98-9452-6900ed3c6c7f" title="test user" subject="Create a comment" date="D:20240306040655+05\'00\'" creationdate="D:20240306040655+05\'00\'" icon="Comment" statemodel="Review"/></add><modify /><delete /></xfdf>',
+          '<?xml version="1.0" encoding="UTF-8" ?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><fields /><add><text xmlns="http://ns.adobe.com/xfdf/" page="5" rect="354.917,1809.8,385.917,1840.8" color="#FFFF00" flags="print,nozoom,norotate" name="3b46c1e1-5afc-8cd2-9195-aa8b2c21fd32" title="test user" subject="Create a comment" date="D:20240306040617+05\'00\'" creationdate="D:20240306040617+05\'00\'" icon="Comment" statemodel="Review"/></add><modify /><delete /></xfdf>',
+        ];
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+        // wait for the document to be fully loaded
+        // this will also fix annotation locations
+        // uncomment the following line to see the expected annotation locations
+        await documentViewer.getDocument().getDocumentCompletePromise();
+
+        const importTasks = stickyAnnotations.map((annotation) =>
+          annotationManager.importAnnotationCommand(annotation)
+        );
+
+        const importedAnnotations = (await Promise.all(importTasks)).flat();
+        annotationManager.drawAnnotationsFromList(importedAnnotations);
+
+        annotationManager.jumpToAnnotation(
+          annotationManager.getAnnotationById(
+            importedAnnotations[importedAnnotations.length - 1].Id
+          )
+        );
       });
     });
   }, []);
